6cd721ea5215650487bd756a670c06492eef9b9d
selil <selil.cp@mindster.in>
2023-06-26 19:24:30 +0100

    add cake listing url into apipath struct

--- CakeItApp/Network/APIPath.swift
+++ CakeItApp/Network/APIPath.swift
@@ -15,4 +15,5 @@ struct APIPath{
         baseURL = AppEnvironment().getBaseURL()
     }
     
+    var getCakeList:String { "\(baseURL)hart88/79a65d27f52cbb74db7df1d200c4212b/raw/ebf57198c7490e42581508f4f40da88b16d784ba/cakeList" }
 }
