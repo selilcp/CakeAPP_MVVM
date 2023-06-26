c8faab160bb38ef23948c7e49d65447e9c62ba91
selil <selil.cp@mindster.in>
2023-06-26 21:14:21 +0100

    Create app navigation controller

---
+++ CakeItApp/View/AppNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  AppNavigationController.swift
+//  CakeItApp
+//
+//  Created by selil on 26/06/23.
+//
+
+import UIKit
+
+class AppNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
